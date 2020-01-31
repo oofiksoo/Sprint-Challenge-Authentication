87600ca897a62e5f14729b2b3d883c1a59177d5d
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-31 10:30:50 -0700

    Working Stretch Client App

---
+++ .client-app/src/components/JokeList.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { displayJokeList } from "../actions/actionCreators";
+import Styled from "styled-components";
+const JokeListContainer = Styled.div`
+display:flex;
+flex-direction:column;
+padding:1%;
+width:80%;
+background-color:#0099cc;
+padding:1%;
+justify-content:center;
+border-radius:20px;
+}
+`;
+const JokeCard = Styled.div`
+display:flex;
+margin:1%;
+width:20%;
+justify-content:center;
+border:2px solid white;
+border-radius:20px;
+}
+`;
+const JokeCardContainer = Styled.div`
+display:flex;
+flex-wrap:wrap;
+margin:1%;
+justify-content:center;
+}
+`;
+class JokeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usertransaction: false
+    };
+  }
+  componentDidMount() {
+    this.props.displayJokeList();
+  }
+  render() {
+    if (!this.props.joketransaction) {
+      return (
+        <div className="status">
+          <h3> Loading Joke Data </h3>
+        </div>
+      );
+    }
+    return (
+      <JokeListContainer>
+        <h1> Current Jokes: </h1>
+        <JokeCardContainer>
+          {this.props.jokes.map(joke => (
+            <JokeCard key={joke.id}>
+              <p> {joke.joke} </p>
+            </JokeCard>
+          ))}
+        </JokeCardContainer>
+      </JokeListContainer>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    jokes: state.jokes,
+    joketransaction: state.joketransaction
+  };
+};
+export default connect(mapStateToProps, { displayJokeList })(JokeList);
---
+++ .client-app/src/reducers/combinedReducer.js
@@ -0,0 +1,75 @@
+import * as types from "../actions/actionTypes";
+
+export const initialState = {
+    username: "",
+    password: "",
+    jokes: [],
+    logintransaction: false,
+    joketransaction: false,
+    token: ""
+};
+
+export const combinedReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.REGISTER:
+            return {...state, token: action.payload };
+        case types.LOGIN:
+            return {
+                ...state,
+                error: "",
+                token: "",
+                logintransaction: false
+            };
+        case types.LOGIN_SUCCESS:
+            return {
+                ...state,
+                logintransaction: true,
+                error: "",
+                token: [...action.payload]
+            };
+
+        case types.LOGIN_FAILED:
+            return {
+                ...state,
+
+                logintransaction: false,
+
+                error: action.payload
+            };
+        case types.GET_JOKES:
+            return {
+                ...state,
+                usertransaction: false,
+                jokes: [],
+                error: ""
+            };
+        case types.GET_JOKES_SUCCESS:
+            return {
+                ...state,
+
+                joketransaction: true,
+
+                error: "",
+
+                jokess: [...state.jokes, ...action.payload]
+            };
+
+        case types.GET_JOKES_FAILED:
+            return {
+                ...state,
+
+                joketransaction: false,
+
+                error: action.payload
+            };
+        case types.LOGOUT:
+            return {
+                ...state,
+                token: ""
+            };
+        default:
+            return state;
+    }
+};
+
+export default [combinedReducer, initialState];
