66e2e442c570addd282e66e81573dd5cffc25ad1
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-31 12:12:06 -0700

    final commit

--- .backend-server/api/server.js
+++ .backend-server/api/server.js
@@ -5,7 +5,7 @@ const helmet = require("helmet");
 const authenticate = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const jokesRouter = require("../jokes/jokes-router.js");
-
+const usersRouter = require("../users/users-router.js");
 const server = express();
 
 server.use(helmet());
@@ -14,6 +14,7 @@ server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/jokes", authenticate, jokesRouter);
+server.use("/api/users", authenticate, usersRouter);
 server.get("/", (req, res) => {
     res.status(200).json({ api: "up" });
 });
