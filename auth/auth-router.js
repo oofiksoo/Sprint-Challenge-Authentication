a6f8e28b3bb9aa48027cd684319040b65150f93d
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-31 09:54:57 -0700

    MVP Met, Register, Login, Get jokes Validated via Postman

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model.js");
+const secrets = require("../config/secrets.js");
 
 router.post("/register", (req, res) => {
     // implement registration
