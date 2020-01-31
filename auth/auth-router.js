2a36da64a84e0fdedd9cfba519eec05c42a1f8ca
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-31 09:41:53 -0700

    Update Package.json, Implement Auth middleware & Auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,54 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+    // implement registration
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({
+                    message: `Welcome ${user.username}!`,
+                    token
+                });
+            } else {
+                res.status(401).json({ message: "You shall not pass!" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        userid: user.id,
+        department: "test"
+    };
+
+    const options = {
+        expiresIn: "1d"
+    };
+
+    return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../config/secrets.js");
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ Error: "Invalid or Expired Token" });
+            } else {
+                req.user = decodedToken.user;
+
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({ you: "shall not pass!" });
+    }
 };
